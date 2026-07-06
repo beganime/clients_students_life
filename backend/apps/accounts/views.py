2ce40845ff92f2a684c0a9bd3eb90b2eabe81d8a
beganime <begenchyagmurow2008@gmail.com>
2026-07-07 00:47:11 +0500

    Import refresh token for logout view

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 
