4d8eadf1a51a5c28095ba83fc20ff2065da0f864
beganime <begenchyagmurow2008@gmail.com>
2026-07-07 00:46:12 +0500

    Add questionnaire submit endpoint

--- backend/apps/questionnaires/urls.py
+++ backend/apps/questionnaires/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MyQuestionnaireAttachmentView, MyQuestionnaireDownloadView, MyQuestionnaireView
+from .views import MyQuestionnaireAttachmentView, MyQuestionnaireDownloadView, MyQuestionnaireSubmitView, MyQuestionnaireView
 
 
 urlpatterns = [
     path('my/', MyQuestionnaireView.as_view(), name='my-questionnaire'),
+    path('my/submit/', MyQuestionnaireSubmitView.as_view(), name='my-questionnaire-submit'),
     path('my/attachments/', MyQuestionnaireAttachmentView.as_view(), name='my-questionnaire-attachments'),
     path('my/download/', MyQuestionnaireDownloadView.as_view(), name='my-questionnaire-download'),
 ]
