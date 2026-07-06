6c25eaeff08345ef343697a79b7a3d0aaca5f68e
beganime <begenchyagmurow2008@gmail.com>
2026-07-07 00:31:00 +0500

    Merge application file review status migrations

---
+++ backend/apps/applications/migrations/0005_merge_applicationfile_review_status.py
@@ -0,0 +1,13 @@
+# Generated manually to merge application migration branches.
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('applications', '0002_applicationfile_review_status'),
+        ('applications', '0004_secure_application_file_names'),
+    ]
+
+    operations = []
